526e374b6404ae065faf87fc962696b195c6e13d
Tyler Veness <calcmogul@gmail.com>
2013-01-18 19:53:24 -0800

    Added Kalman Filter class

---
+++ KalmanFilter.cpp
@@ -0,0 +1,16 @@
+//=============================================================================
+//File Name: KalmanFilter.cpp
+//Description: Provides an interface to a Kalman Filter for cleaning up gyro
+//             noise among other real-time data signals
+//Author: FRC Team 3512, Spartatroniks
+//=============================================================================
+
+#include "KalmanFilter.hpp"
+
+KalmanFilter::KalmanFilter() {
+
+}
+
+KalmanFilter::~KalmanFilter() {
+
+}
